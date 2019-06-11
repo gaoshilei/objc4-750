26c0572015f01ece8deb8733e79a9b25a0ad6f1b
gaoshilei <goslei@163.com>
2019-06-11 18:16:06 +0800

    commit

---
+++ runtimeTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  runtimeTest
+//
+//  Created by gaoshilei on 2019/6/11.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
